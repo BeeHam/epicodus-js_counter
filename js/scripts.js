967e2f4fd4aa3e12bc71ddc22e799eaea4542140
Tony Navarro & Brigham Thompson <student@epicodus.com>
2015-09-10 11:58:50 -0700

    clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,24 @@
-
-
-var countUpBy = function(ceil, countBy) {             //begin function
+//program logic
+var countUpBy = function(ceil, countBy) {
   var resultSet = [];
-  for (var i = countBy; i <= ceil; i += countBy) {    //begin loop
+
+  for (var i = countBy; i <= ceil; i += countBy) {
     resultSet.push(i);
-    // $("ul#count").append("<li>" + i + "</li>");       //populate list
   }
+
   return resultSet;
 };
 
+
+//client logic
 $(document).ready(function() {
-
-  $('form#counter').submit(function(event) {              //submit button
-  var countUpTo = parseInt($("input#ceil").val());                  //grab from form
-  var countCeil = parseInt($("input#countBy").val());            //grab from form
-
-
+  $('form#counter').submit(function(event) {
+  var countUpTo = parseInt($("input#ceil").val());
+  var countCeil = parseInt($("input#countBy").val());
   var result = countUpBy(countUpTo, countCeil);
-  //
   for (var i in result) {
     $("ul#count").append("<li>" + result[i] + "</li>");
   };
-  //
-  // $('#count').text(result);
   $('#result').show();
   event.preventDefault();
   });
